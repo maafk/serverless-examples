571774b5215c4f792cdc42416902ba6fb915aa7a
Taylor <taylor@taylorondrey.com>
2018-03-12 06:36:42 -0400

    added messaging about connectiontimeouts

--- serverless-framework/csv-processing/README.md
+++ serverless-framework/csv-processing/README.md
@@ -1,1 +1,19 @@
-README.md
+# Processing large CSV files using lambda
+
+Say you havea large text file for processing.
+
+You could go through line by line and process/validate each line.
+
+The idea here is to break up large files into smaller chunks, and process each chunk using lambda
+
+In this project, I used a sample csv file with 5 million lines.
+
+I have an S3 bucket called maafkn-csv-processor
+
+The only folder I create is one called `raw`.
+
+Once a csv file is copied there, the chain reaction starts.
+
+- The `chunk` lambda separates the file saved to `raw` into lines of 10,000 and saves to the `chunked` folder
+- Once a file is saved in `chunked`, the `processChunk` lambda opens the file, validates the email address is valid, and encrypts the SSN, then saves to the `processed` folder.
+- Once a file is saved to the `processed` folder, the `saveChunk` lambda kicks in and adds to a Postgres RDS instance using the [`copy_from`](http://initd.org/psycopg/docs/cursor.html#cursor.copy_from) command
--- serverless-framework/csv-processing/saveChunk.py
+++ serverless-framework/csv-processing/saveChunk.py
@@ -2,25 +2,39 @@ import boto3
 import psycopg2
 from smart_open import smart_open
 import os
+import sys
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 bucket = os.environ['bucket']
 s3 = boto3.client('s3')
 
-conn = psycopg2.connect(
-    host=os.environ['DB_HOST'],
-    port='5432',
-    user=os.environ['DB_USER'],
-    password=os.environ['DB_PASS'],
-    dbname='csv_process'
-)
+try:
+    conn = psycopg2.connect(
+        host=os.environ['DB_HOST'],
+        port='5432',
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASS'],
+        dbname='csv_process',
+        connect_timeout=5
+    )
+except:
+    logger.error(
+        "ERROR: Unexpected error: Could not connect to Postgres instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS Postgres instance succeeded")
 
 cur = conn.cursor()
 db_table = 'artists'
 
 
 def copy_from_s3_file_to_db(key):
-    with smart_open(key, 'r') as fh:
-        cur.copy_from(fh, db_table, sep=',')
+    f = smart_open(key, 'r')
+    cur.copy_from(f, db_table, sep=',')
+    f.close()
 
 
 def get_list_of_s3_keys_from_event(event):
